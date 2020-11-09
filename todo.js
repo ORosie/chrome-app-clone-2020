d7d06f614a8dc95dcffad71dad7e84466e45f811
s00 <Orosie230@gmail.com>
2020-11-09 23:57:17 +0900

    making to-dos

---
+++ todo.js
@@ -0,0 +1,61 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+//class name가져올때 점!! 잘 찍자
+
+const TODOS_LS = "toDos";
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+  //JSON.stringify는 자바스크립트 object를 string으로 바꿔준다.
+}
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  // list 말고 이름이 뭐가 되든 상관없음.
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "❌";
+  //이모티콘 넣는 단축키 cmd+control+spacebar
+  const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  span.innerText = text;
+
+  toDoList.appendChild(li);
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  li.id = newId;
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo) {
+      paintToDo(toDo.text);
+    });
+    //forEach는 array에 담겨있는 것들을 각각에 한번씩 함수를 실행시켜줌
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
